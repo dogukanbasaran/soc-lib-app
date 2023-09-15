fb23bf715856d8e68c4b2245ce0bc00843752546
Dogukan Basaran <dogukanbasarann@hotmail.com>
2023-09-15 22:07:36 +0300

    Ufak bir RWD değişikliği yapildi.

--- src/Components/Introduction.jsx
+++ src/Components/Introduction.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Introduction() {
   return (
     <div className='h- bg-gradient-to-b from-slate-900 to-black'>
-        <h2 className='font-bold text-4xl lg:text-5xl xl:text-6xl text-white flex justify-center mb-5 pt-5'>Start sociology from here.</h2>
-        <p className=' lg:text-lg xl:text-xl text-gray-300 flex justify-center'>A <i className='text-blue-400 px-2'>sociology library </i> open to everyone.</p>
+        <h2 className='font-bold text-2xl sm:text-4xl lg:text-5xl xl:text-6xl text-white flex justify-center mb-0 sm:mb-5 pt-5'>Start sociology from here.</h2>
+        <p className='lg:text-lg xl:text-xl text-gray-300 flex justify-center'>A <i className='text-blue-400 px-2'>sociology library </i> open to everyone.</p>
     </div>
   )
 }
